543e0b5442ba2bd307501af2400d3895f3d5c7d4
Alzidan <71370680+Alzidan@users.noreply.github.com>
2020-10-27 23:42:01 +0700

    Add files via upload

---
+++ terminal/console.java
@@ -0,0 +1,13 @@
+package com.terminal;
+
+public class console {
+
+    public static void log(String message) {
+        System.out.println(message);
+    }
+class terminal {
+        public static void log(String message) {
+            System.out.println(message);
+        }
+}
+}
